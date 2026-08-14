bdac6dd1b91c696b2e0621eb60271968e52e16d5
Mmesoma-Constance <mmesomanwokem@gmail.com>
2026-08-14 14:08:43 +0100

    feat: BookList data

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- images: {
-  remotePatterns: [
-    {
-      protocol: "https",
-      hostname: "placehold.co",
-    },
-    {
-      protocol: "https",
-      hostname: "m.media-amazon.com",
-    },
-  ],
- },
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "m.media-amazon.com" },
+      { protocol: "https", hostname: "placehold.co" },
+    ],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
